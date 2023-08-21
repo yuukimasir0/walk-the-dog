3915741df3c7cfc40f226d215e0fa52366ba3cde
yuukimasir0 <plusion00@icloud.com>
2023-08-21 21:54:37 +0900

    Fixed state

--- src/game.rs
+++ src/game.rs
@@ -29,14 +29,18 @@ mod red_hat_boy_states {
     const FLOOR: i16 = 475;
     const IDLE_FRAME_NAME: &str = "Idle";
     const RUN_FRAME_NAME: &str = "Run";
+    const SLIDE_FRAME_NAME: &str = "Slide";
     const IDLE_FRAMES: u8 = 29;
     const RUN_FRAMES: u8 = 23;
+    const SLIDE_FRAMES: u8 = 14;
     const RUNNING_SPEED: i16 = 3;
 
     #[derive(Clone, Copy)]
     pub struct Idle;
     #[derive(Clone, Copy)]
     pub struct Running;
+     #[derive(Clone, Copy)]
+    pub struct Sliding;
 
     #[derive(Clone, Copy)]
     pub struct RedHatBoyContext {
@@ -76,6 +80,13 @@ mod red_hat_boy_states {
             }
         }
 
+        pub fn slide(self) -> RedHatBoyState<Sliding> {
+            RedHatBoyState {
+                context: self.context.reset_frame(),
+                _state: Sliding {},
+            }
+        }
+
         pub fn frame_name(&self) -> &str {
             IDLE_FRAME_NAME
         }
@@ -91,6 +102,19 @@ mod red_hat_boy_states {
         }
         pub fn update(&mut self) {
             self.context = self.context.update(RUN_FRAMES);
+        }
+
+        pub fn slide(self) -> RedHatBoyState<Sliding> {
+            RedHatBoyState { context: self.context.reset_frame(), _state: Sliding {} }
+        }
+    }
+
+    impl RedHatBoyState<Sliding> {
+        pub fn frame_name(&self) -> &str {
+            SLIDE_FRAME_NAME
+        }
+        pub fn update(&mut self) {
+            self.context = self.context.update(SLIDE_FRAMES);
         }
     }
 
@@ -122,16 +146,19 @@ mod red_hat_boy_states {
 enum RedHatBoyStateMachine {
     Idle(RedHatBoyState<Idle>),
     Running(RedHatBoyState<Running>),
+    Sliding(RedHatBoyState<Sliding>),
 }
 
 pub enum Event {
     Run,
+    Slide,
 }
 
 impl RedHatBoyStateMachine {
     fn transition(self, event: Event) -> Self {
         match (self, event) {
             (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
+            (RedHatBoyStateMachine::Running(state), Event::Slide) => state.slide().into(),
             _ => self,
         }
     }
@@ -140,6 +167,7 @@ impl RedHatBoyStateMachine {
         match self {
             RedHatBoyStateMachine::Idle(state) => state.frame_name(),
             RedHatBoyStateMachine::Running(state) => state.frame_name(),
+            RedHatBoyStateMachine::Sliding(state) => state.frame_name(),
         }
     }
 
@@ -147,6 +175,7 @@ impl RedHatBoyStateMachine {
         match self {
             RedHatBoyStateMachine::Idle(state) => &state.context(),
             RedHatBoyStateMachine::Running(state) => &state.context(),
+            RedHatBoyStateMachine::Sliding(state) => &state.context(),
         }
     }
 
@@ -160,6 +189,10 @@ impl RedHatBoyStateMachine {
                 state.update();
                 RedHatBoyStateMachine::Running(state)
             }
+            RedHatBoyStateMachine::Sliding(mut state) => {
+                state.update();
+                RedHatBoyStateMachine::Sliding(state)
+            }
         }
     }
 }
@@ -167,6 +200,12 @@ impl RedHatBoyStateMachine {
 impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
     fn from(state: RedHatBoyState<Running>) -> Self {
         RedHatBoyStateMachine::Running(state)
+    }
+}
+
+impl From<RedHatBoyState<Sliding>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Sliding>) -> Self {
+        RedHatBoyStateMachine::Sliding(state)
     }
 }
 
@@ -222,6 +261,10 @@ impl RedHatBoy {
     fn run_right(&mut self) {
         self.state_machine = self.state_machine.transition(Event::Run);
     }
+
+    fn slide(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Slide);
+    }
 }
 
 #[async_trait(?Send)]
@@ -242,6 +285,7 @@ impl Game for WalkTheDog {
 
     fn update(&mut self, keystate: &KeyState) {
         if keystate.is_pressed("ArrowDown") {
+            self.rhb.as_mut().unwrap().slide();
         }
         if keystate.is_pressed("ArrowUp") {
         }
